5f8d63d25fdc727a68377c2ddeff1adb26a9bb78
Joshua Kaasik <joshua.kaasik@gmail.com>
2024-04-23 13:45:14 -0700

    Added piece logic pseudocode

--- guppy/board.py
+++ guppy/board.py
@@ -1,19 +1,34 @@
-class Board:
-    def __init__(self):
-        self.white_pawns = 0
-        self.black_pawns = 0
-        self.white_knights = 0
-        self.black_knights = 0
-        self.white_bishops = 0
-        self.black_bishops = 0
-        self.white_rooks = 0
-        self.black_rooks = 0
-        self.white_queens = 0
-        self.black_queens = 0
-        self.white_king = 0
-        self.black_king = 0
+def get_coordinate_dictionary():
+    coordinate_dict = {}
+    for i in range(64):
+        row = (i + 8) // 8
+        column = i % 8
+        columns = "abcdefgh"
+        coordinate_dict[i] = f"{columns[column]}{row}"
+    return coordinate_dict
 
-    
+
+class Board:
+
+    COORDINATES = get_coordinate_dictionary()
+    INDICES = {v: k for k, v in COORDINATES.items()}
+    ICONS = {
+        'wp': '♟︎',
+        'wN': '♞',
+        'wB': '♝',
+        'wR': '♜',
+        'wQ': '♛',
+        'wK': '♚',
+        'bp': '♙',
+        'bN': '♘',
+        'bB': '♗',
+        'bR': '♖',
+        'bQ': '♕',
+        'bK': '♔',
+        '.': '.'
+    }
+
+
     def setup_board(self):
         self.black_pawns = 0x00FF000000000000 
         self.black_rooks = (1 << 56) | (1 << 63)
@@ -29,42 +44,57 @@ class Board:
         self.white_queens = (1 << 3)
         self.white_king = (1 << 4)
 
-
+    
     def display_board(self):
         print("  a b c d e f g h")
         for row in range(8):
             print(8 - row, end=' ')
             for col in range(8, 0, -1):
                 square_index = abs(64 - (row * 8 + col))
-                piece_icon = '.'
-                if self.black_pawns & (1 << square_index):
-                    piece_icon = '♙'
-                elif self.white_pawns & (1 << square_index):
-                    piece_icon = '♟︎'
-                elif self.black_knights & (1 << square_index):
-                    piece_icon = '♘'
-                elif self.white_knights & (1 << square_index):
-                    piece_icon = '♞'
-                elif self.black_bishops & (1 << square_index):
-                    piece_icon = '♗'
-                elif self.white_bishops & (1 << square_index):
-                    piece_icon = '♝'
-                elif self.black_rooks & (1 << square_index):
-                    piece_icon = '♖'
-                elif self.white_rooks & (1 << square_index):
-                    piece_icon = '♜'
-                elif self.black_queens & (1 << square_index):
-                    piece_icon = '♕'
-                elif self.white_queens & (1 << square_index):
-                    piece_icon = '♛'
-                elif self.black_king & (1 << square_index):
-                    piece_icon = '♔'
-                elif self.white_king & (1 << square_index):
-                    piece_icon = '♚'
+                piece = self.identify_piece(square_index)
+                piece_icon = self.ICONS[piece]
                 print(piece_icon, end=' ')
             print()  # Newline after each row
         print()  # Extra newline for better separation
-    
+
+    def identify_piece(self, position):
+        if isinstance(position, str):
+            position = self.INDICES[position]
+
+        if self.black_pawns & (1 << position):
+            piece = "bp"
+        elif self.white_pawns & (1 << position):
+            piece = "wp"
+        elif self.black_knights & (1 << position):
+            piece = "bN"
+        elif self.white_knights & (1 << position):
+            piece = "wN"
+        elif self.black_bishops & (1 << position):
+            piece = "bB"
+        elif self.white_bishops & (1 << position):
+            piece = "wB"
+        elif self.black_rooks & (1 << position):
+            piece = "bR"
+        elif self.white_rooks & (1 << position):
+            piece = "wR"
+        elif self.black_queens & (1 << position):
+            piece = "bQ"
+        elif self.white_queens & (1 << position):
+            piece = "wQ"
+        elif self.black_king & (1 << position):
+            piece = "bK"
+        elif self.white_king & (1 << position):
+            piece = "wK"
+        else:
+            piece = "."
+        
+        return piece
+        
+        
+
+
+
+            
 
 def main():
     chess = Board()
---
+++ guppy/pieces.py
@@ -0,0 +1,33 @@
+"""
+Bitwise movement
+
+pawns: +/-8 or +/-16; +/-7 or +/-9 when capturing
+knights: +/- [6, 15, 17, 10]
+bishops: +/- multiples of 9, +/- multiples of 7
+rooks: +/- multiples of 1 (max 7); +/- multiples of 8
+queens: +/- multiples of 1, 7, 8, 9
+kings: +/- [1, 7, 8, 9]
+
+"""
+
+"""
+Edge constraints
+
+knight: sum(abs(change_row + change_column)) == 3
+bishop: abs(change_row + change_column) % 2 == 0
+rook
+queen
+
+
+"""
+
+def build_lookup():
+    move_lookup = {}
+
+    for i in range(64):
+        row = i // 8
+        column = i % 8
+        for p in ["bishop"]:
+            moves = []
+            for r in range(1, row+1)
+            # maybe the minimum of row and column should determine range?
